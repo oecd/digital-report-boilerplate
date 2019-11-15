14b0aac13db2d6141c52bb3444cd5c2a9d2b88ef
Bertrand Riviere <bertrand.riviere@gmail.com>
2019-11-15 16:54:56 +0100

    refs #23659 - implement a custom dropdown-like (that is always opened) for large screen (with filter feature)

--- src/modules/projections-by-country/projections-by-country.js
+++ src/modules/projections-by-country/projections-by-country.js
@@ -7,35 +7,81 @@ import { getResourceByNsAndKey } from '../../localization';
 import data from './projections-by-country-data.json';
 import './projections-by-country.scss';
 
+const selectCountryConstantTitle = getResourceByNsAndKey('projections-by-country', 'select-country-constant-title');
+$('#projections-by-country-custom-opened-select-search-input').attr('placeholder', selectCountryConstantTitle);
+
 const countrySelected = (country) => {
   $('#projections-by-country-country-title').text(country[appConfig.lang].label);
   $('#projections-by-country-dp-iframe').attr('src', country[appConfig.lang].dataPortalUrl);
   $('#projections-by-country-read-button').attr('href', country[appConfig.lang].webBookUrl);
   $('#projections-by-country-projection-button').attr('href', country[appConfig.lang].oecdOrgUrl);
-}
 
-const createSelectWithDataSizeBasedOnWindowWidth = () => {
-  $('#projections-by-country-select').selectpicker({ size: window.innerWidth < 992 ? 5 : 13 });
+  $('.projections-by-country__custom-opened-select-list').find('.dropdown-item').removeClass('active');
+  $('.projections-by-country__custom-opened-select-list').find(`[data-id='${country.id}']`).addClass('active');
 };
 
-const selectCountryConstantTitle = getResourceByNsAndKey('projections-by-country', 'select-country-constant-title');
+const customOpenedSelectCountrySelected = (e) => {
+  const selectedCountry = _.find(data, { 'id': $(e.currentTarget).attr('data-id') });
+  countrySelected(selectedCountry);
+  $('#projections-by-country-select').selectpicker('val', selectedCountry.id);
+};
+
+const customOpenedSelectScrollToCountry = (country) => {
+  const listOffsetTop = $('.projections-by-country__custom-opened-select-list-container').offset().top;
+  const selectedOffsetTop = $('.projections-by-country__custom-opened-select-list').find(`[data-id='${country.id}']`).offset().top;
+  $('.projections-by-country__custom-opened-select-list-container').scrollTop(selectedOffsetTop - listOffsetTop);
+};
+
+const customOpenedSelectCountryFiltered = (e) => {
+  const filterText = _.toLower(e.currentTarget.value);
+  if (filterText === '') {
+    $('.projections-by-country__custom-opened-select-list').find('.dropdown-item').css('display', 'block');
+    return;
+  }
+  const containsFilterText = (text) => _.includes(text, filterText);
+  _.forEach(
+    $('.projections-by-country__custom-opened-select-list').find('.dropdown-item'),
+    (item) => {
+      const itemText = _.toLower($(item).text());
+      $(item).css('display', _.includes(itemText, filterText) ? 'block' : 'none');
+    },
+  );
+}
+
+const customOpenedSelectSelectFirstItemIfEnter = (e) => {
+  if (e.keyCode !== 13 || e.currentTarget.value === '') {
+    return;
+  }
+
+  const firstFilteredCountry = _.find(
+    $('.projections-by-country__custom-opened-select-list').find('.dropdown-item'),
+    (item) => $(item).css('display') === 'block',
+  );
+
+  if (firstFilteredCountry) {
+    $(firstFilteredCountry).trigger('click');
+  }
+};
+
 
 _.forEach(
   _.sortBy(data, [(c) => c[appConfig.lang].label]),
   (country) => {
     $('#projections-by-country-select')
       .append(`<option value="${country.id}" title="${selectCountryConstantTitle}">${country[appConfig.lang].label}</option>`);
+    $('.projections-by-country__custom-opened-select-list')
+      .append(`<li><a data-id="${country.id}" role="option" class="dropdown-item" tabindex="0">${country[appConfig.lang].label}</a></li>`);
   },
 );
 
-window.addEventListener('resize', () => {
-  createSelectWithDataSizeBasedOnWindowWidth();
-});
-createSelectWithDataSizeBasedOnWindowWidth();
+$('.projections-by-country__custom-opened-select-list').find('.dropdown-item').on('click', customOpenedSelectCountrySelected);
+$('#projections-by-country-custom-opened-select-search-input').on('input', customOpenedSelectCountryFiltered);
+$('#projections-by-country-custom-opened-select-search-input').on('keyup', customOpenedSelectSelectFirstItemIfEnter);
 
 const defaultCountry = _.find(data, { isDefault: true }) || _.head(data);
 countrySelected(defaultCountry);
 $('#projections-by-country-select').selectpicker('val', defaultCountry.id);
+customOpenedSelectScrollToCountry(defaultCountry)
 
 $('#projections-by-country-select').on('change', (e) => {
   const selectedCountry = _.find(data, { id: e.currentTarget.value });
