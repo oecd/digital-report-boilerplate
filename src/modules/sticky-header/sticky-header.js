e7fe3f6d41f5b4a9f0ef19df7560a2362cbccbb9
Bertrand Riviere <bertrand.riviere@gmail.com>
2020-02-10 15:06:43 +0100

    refs #24832 - concat hash in lang urls

--- src/modules/sticky-header/sticky-header.js
+++ src/modules/sticky-header/sticky-header.js
@@ -8,9 +8,9 @@ const burgerButton = navbar.find('.internal-nav__toggle');
 const langSelection = navbar.find('.internal-nav__lang-sel');
 let sticky = navbar.offset().top + 5;
 
-$('.url-en-sticky-header').attr('href', appConfig.urls.en)
-$('.url-fr-sticky-header').attr('href', appConfig.urls.fr)
-$('.url-es-sticky-header').attr('href', appConfig.urls.es)
+$('.url-en-sticky-header').attr('href', `${appConfig.urls.en}${document.location.hash}`)
+$('.url-fr-sticky-header').attr('href', `${appConfig.urls.fr}${document.location.hash}`)
+$('.url-es-sticky-header').attr('href', `${appConfig.urls.es}${document.location.hash}`)
 
 const reactToPageYOffsetChange = () => {
   if (window.pageYOffset >= sticky) {
